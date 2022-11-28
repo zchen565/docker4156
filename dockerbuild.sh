34e93ddb9c0a868c6ac0b9c1cf84a999cafe9ce2
zihao <zihao@LAPTOP-K59Q4A7L.localdomain>
2022-11-28 18:28:49 -0500

    old version

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -6,4 +6,4 @@ GROUPNAME=`id -g -n`
 MY_UID=`id -u`
 MY_GID=`id -g`
 
-docker build -t final --build-arg UNAME=${USERNAME} --build-arg GNAME=${GROUPNAME} --build-arg UID=${MY_UID} --build-arg GID=${MY_GID} .
+docker build -t old --build-arg UNAME=${USERNAME} --build-arg GNAME=${GROUPNAME} --build-arg UID=${MY_UID} --build-arg GID=${MY_GID} .
